22447cb03fc07352db10c99554f4e704b0c1deef
Abrams27 <abramowiczmarcin99@gmail.com>
2020-01-24 17:33:36 +0100

    init

---
+++ app/src/main/java/touk/recruitment/task/TouKRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package touk.recruitment.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouKRecruitmentTaskApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TouKRecruitmentTaskApplication.class, args);
+  }
+
+}
